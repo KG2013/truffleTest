ce076c77d2db3c378eac33d34d4d03c6f376e775
zhangkuo <zhangkuo2000@163.com>
2022-05-12 19:23:06 +0800

    test

--- migrations/2_hello_world_migration.js
+++ migrations/2_hello_world_migration.js
@@ -2,9 +2,9 @@ const HelloWorld = artifacts.require("HelloWorld");
 const HelloWorldPermanent = artifacts.require("HelloWorldPermanent");
 
 module.exports = function (deployer) {
-  deployer.deploy(HelloWorld,"Hello world constructor").then( async () => {
+  deployer.deploy(HelloWorld,"hi KG").then( async () => {
     let instance = await HelloWorld.deployed();
     let message = instance.message();
-    deployer.deploy(HelloWorldPermanent,message);
+    return deployer.deploy(HelloWorldPermanent,message);
   });
 };
